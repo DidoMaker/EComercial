2f231ec4cd8267ff0cafa0596681654b3886949a
giang <gnaig1510@gmail.com>
2024-05-23 21:14:55 +0700

    LoginForm

---
+++ src/container/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginForm from '../Components/LoginForm/LoginForm';
+import './App.css';
+
+function App() {
+    return(
+        <div>
+            <LoginForm />
+        </div>
+    );
+};
+
+
+export default App;
